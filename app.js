d7ebda660ad66b8b28bc5ff202aef1f9eb04f9eb
lyhcode <lyhcode@gmail.com>
2015-10-02 16:10:29 +0800

    add fb and google analytics

--- app.js
+++ app.js
@@ -12,8 +12,6 @@ var session = require('express-session');
 var flash = require('connect-flash');
 var app = express();
 
-
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
