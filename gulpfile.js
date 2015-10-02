224cace082db7e2cb72e07ba61b6706b75ffcdcb
lyhcode <lyhcode@gmail.com>
2015-10-02 20:40:45 +0800

    image size optimize

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,17 @@
 var gulp = require('gulp');
 var size = require('gulp-size');
 var notify = require('gulp-notify');
+var foreach = require('gulp-foreach');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
 var imageResize = require('gulp-image-resize');
 var parallel = require("concurrent-transform");
 var os = require("os");
 
 gulp.task('default', function () {
-    var s = size({
-      showFiles: true,
-      pretty: true
-    });
 
     return gulp.src('./public/images-origin/**/*.{png,jpg}')
-    .pipe(s)
+    .pipe(size({showFiles: true}))
     .pipe(parallel(imageResize({
       width : 2560,
       height : 1600,
@@ -20,11 +19,11 @@ gulp.task('default', function () {
       quality: 0.6,
       imageMagick: true
     }), os.cpus().length))
-    .pipe(notify({
-        onLast: true,
-        message: function () {
-            return 'Total size ' + s.prettySize;
-        }
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: false}],
+        use: [pngquant()]
     }))
-    .pipe(gulp.dest('./public/images'));
+    .pipe(gulp.dest('./public/images'))
+    .pipe(size({showFiles: true}));
 });
