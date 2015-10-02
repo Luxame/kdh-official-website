9029e7137b3256dc0b4e8be768423b3133692859
lyhcode <lyhcode@gmail.com>
2015-10-02 19:59:22 +0800

    images update

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,30 @@
 var gulp = require('gulp');
+var size = require('gulp-size');
+var notify = require('gulp-notify');
 var imageResize = require('gulp-image-resize');
+var parallel = require("concurrent-transform");
+var os = require("os");
 
 gulp.task('default', function () {
-    gulp.src(['./public/images-origin/**/*.png','./public/images-origin/**/*.jpeg'])
-    .pipe(imageResize({
+    var s = size({
+      showFiles: true,
+      pretty: true
+    });
+
+    return gulp.src('./public/images-origin/**/*.{png,jpg}')
+    .pipe(s)
+    .pipe(parallel(imageResize({
       width : 2560,
       height : 1600,
       upscale : false,
       quality: 0.6,
       imageMagick: true
+    }), os.cpus().length))
+    .pipe(notify({
+        onLast: true,
+        message: function () {
+            return 'Total size ' + s.prettySize;
+        }
     }))
     .pipe(gulp.dest('./public/images'));
 });
