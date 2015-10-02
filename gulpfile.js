be78f43eac3c5d0a13622c3b277478185c39339c
fuyaode <dan82625@gmail.com>
2015-10-02 18:12:46 +0800

    fix File Format

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,12 @@ var gulp = require('gulp');
 var imageResize = require('gulp-image-resize');
 
 gulp.task('default', function () {
-    gulp.src('./public/images/**/*.png')
+    gulp.src(['./public/images-origin/**/*.png','./public/images-origin/**/*.jpeg'])
     .pipe(imageResize({
       width : 2560,
       height : 1600,
       upscale : false,
       quality: 0.6,
-      format : 'jpeg'
     }))
     .pipe(gulp.dest('./public/images'));
 });
